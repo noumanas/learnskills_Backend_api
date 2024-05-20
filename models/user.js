f4bcb3edc228023563f155c82e867addb82a1536
nouman qureshi <noumanqureshi15@gmail.com>
2024-05-20 21:20:10 +0500

    profile

--- models/user.js
+++ models/user.js
@@ -79,6 +79,7 @@ const userSchema = new mongoose.Schema({
   },
   profile:{
     type:String,
+    default:""
   },
   createdDate: { // New field for recording the date of creation
     type: Date,
